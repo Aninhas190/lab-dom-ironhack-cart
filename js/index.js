094935420afdc72fc364b158cc83892cf5e9b0e2
Ana de Almeida <ana.isabel190@gmail.com>
2020-04-21 17:05:03 +0100

    stuck at iteration 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  
+  console.log('Calculating subtotal, yey!');
+
+  //... your code goes here
   //extracting specific values from document
   const $price = document.querySelector('.price span').innerHTML;
   const $quantity = document.querySelector('.quantity input').value;
@@ -9,23 +13,28 @@ function updateSubtotal(product) {
   //create variable where it would hold subtotal on the document
   let $subtotal = document.querySelector('.subtotal span');
   //return subtotal value to document
-  $subtotal.innerHTML = subtotal;
-  return subtotal;
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  
+  return $subtotal.innerHTML = subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product span');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelectorAll('.product span');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const $product = document.querySelectorAll('.product');
+  let newsubtotal;
+  for (let i = 0; i < $product.length; i++) {
+    newsubtotal = updateSubtotal($product[i]);
+    
+  }
+  
 
+  return newsubtotal;
   // ITERATION 3
   //... your code goes here
 }
